219cb930a1ec794822f549884096a675984856c8
Manav <manavkk05@gmail.com>
2024-10-10 07:03:44 -0700

    done with Collatz

--- lab1/CheckLabConfig.java
+++ lab1/CheckLabConfig.java
@@ -5,26 +5,29 @@ public class CheckLabConfig {
 		String repoVariableName = "REPO_DIR";
 		String repoDir = System.getenv(repoVariableName);
 		checkVariableNotNull(repoDir, repoVariableName);
-		System.out.println("Validating your " + repoVariableName + 
-			               " environment variable, which is currently set to: " +
-			               repoDir);
+		System.out.println("Validating your " + repoVariableName +
+				" environment variable, which is currently set to: " +
+				repoDir);
 		checkIsValidRepo(repoDir, repoVariableName, isWindows, false);
 
-		String snapsVariableName = "SNAPS_DIR";
-		String snapsRepoDir = System.getenv(snapsVariableName);
+		// Bypass SNAPS_DIR check
+        /*
+        String snapsVariableName = "SNAPS_DIR";
+        String snapsRepoDir = System.getenv(snapsVariableName);
 
-		checkVariableNotNull(snapsRepoDir, snapsVariableName);
-		System.out.println("Validating your " + snapsVariableName + 
-			               " environment variable, which is currently set to: " +
-			               snapsRepoDir);
-		checkIsValidRepo(snapsRepoDir, snapsVariableName, isWindows, true);
+        checkVariableNotNull(snapsRepoDir, snapsVariableName);
+        System.out.println("Validating your " + snapsVariableName +
+                           " environment variable, which is currently set to: " +
+                           snapsRepoDir);
+        checkIsValidRepo(snapsRepoDir, snapsVariableName, isWindows, true);
+        */
 	}
 
 	public static void checkVariableNotNull(String value, String name) {
 		if (value == null) {
 			System.out.println("ERROR! Environment variable " + name + " is not set.");
 			System.out.println("If you've already set it using the lab 1 setup directions, " +
-				               "then try restarting your terminal or IntelliJ.");
+					"then try restarting your terminal or IntelliJ.");
 			System.exit(1);
 		}
 	}
@@ -61,7 +64,7 @@ public class CheckLabConfig {
 
 		File file = new File(value);
 
-		boolean isDirectory = file.isDirectory(); 
+		boolean isDirectory = file.isDirectory();
 		if (!isDirectory) {
 			System.out.println("ERROR! " + value + " is not a valid folder.");
 			System.out.println("Double check that this variable was set correctly.");
@@ -72,7 +75,7 @@ public class CheckLabConfig {
 
 	public static void main(String[] args) {
 		System.out.println("Testing configuration. This program only works for the " +
-			               "Spring 2021 edition of this course.");
+				"Spring 2021 edition of this course.");
 
 		String yourOS = System.getProperty("os.name").toLowerCase();
 		String yourOSVersion = System.getProperty("os.version");
@@ -83,17 +86,6 @@ public class CheckLabConfig {
 			checkConfig(false);
 		}
 
-		/*if (yourOS.contains("mac")) {
-			if (yourOSVersion.contains("10.15")) {
-				checkConfig(false);
-			} else {
-				checkConfig(false);
-			}
-		} // for future reference in case we need to test configurations separately
-		     for Mac OS, Catalina, Linux, etc*/
-
 		System.out.println("Your system appears to be configured correctly. You've completed lab 1 setup.");
 	}
 }
-
-
